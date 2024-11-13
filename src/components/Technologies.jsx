b084ea2ac7adaf1b86304b9d5f56d99e5b34a01d
Ice Kittipong <ammice1122@gmail.com>
2024-11-13 18:07:13 +0700

    portfolio ice

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,6 +9,8 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { IoLogoPython } from "react-icons/io";
 import { SiExpress } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 const iconVariants = (duration) => ({
   initial: { y: -10 },
@@ -60,12 +62,28 @@ export default function Technologies() {
           <IoLogoJavascript className="text-7xl text-yellow-400" />
         </motion.div>
         <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaBootstrap className="text-7xl text-purple-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiTailwindCssFill className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-          <IoLogoPython className="text-7xl" />
+          <IoLogoPython className="text-7xl text-yellow-400" />
         </motion.div>
         <motion.div
           variants={iconVariants(5)}
