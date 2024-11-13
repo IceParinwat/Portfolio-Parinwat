8dbd0f72d3d076a7b3788bb9101de2754bf69526
Ice Kittipong <ammice1122@gmail.com>
2024-11-13 17:59:25 +0700

    portfolio ice

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,14 @@
 import { RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
 import { SiMongodb } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { motion } from "motion/react";
+import { TiHtml5 } from "react-icons/ti";
+import { DiCss3 } from "react-icons/di";
+import { IoLogoJavascript } from "react-icons/io5";
+import { IoLogoPython } from "react-icons/io";
+import { SiExpress } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
 
 const iconVariants = (duration) => ({
   initial: { y: -10 },
@@ -29,19 +34,22 @@ export default function Technologies() {
       >
         Technologies
       </motion.h1>
-      <motion.div
-        whileInView={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 1.5 }}
-        className="flex flex-wrap items-center justify-center gap-4"
-      >
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TiHtml5 className="text-7xl text-orange-500" />
+        </motion.div>
         <motion.div
           variants={iconVariants(2.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-          <RiReactjsLine className="text-7xl text-cyan-400" />
+          <DiCss3 className="text-7xl text-blue-500" />
         </motion.div>
         <motion.div
           variants={iconVariants(3)}
@@ -49,10 +57,42 @@ export default function Technologies() {
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-          <TbBrandNextjs className="text-7xl" />
+          <IoLogoJavascript className="text-7xl text-yellow-400" />
         </motion.div>
         <motion.div
-          variants={iconVariants(2)}
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoPython className="text-7xl" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiExpress className="text-7xl" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMysql className="text-7xl text-yellow-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -75,7 +115,7 @@ export default function Technologies() {
         >
           <BiLogoPostgresql className="text-7xl text-sky-400" />
         </motion.div>
-      </motion.div>
+      </div>
     </div>
   );
 }
