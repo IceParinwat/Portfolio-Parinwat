f62cd8750fd0712c0503294a22c7d74fd406e2c0
Ice Kittipong <ammice1122@gmail.com>
2024-11-15 10:45:51 +0700

    edit offset ScollY

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
+const scrollWithOffset = (id) => {
+  const element = document.getElementById(id);
+  const offset = 100;
+  const elementPosition = element.getBoundingClientRect().top;
+  const offsetPosition = elementPosition + window.scrollY - offset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+};
+
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleClick = (e, id) => {
+    e.preventDefault();
+    scrollWithOffset(id.substring(1));
+    setIsMenuOpen(false);
   };
 
   return (
@@ -14,7 +32,7 @@ export default function Navbar() {
         <div className="relative flex items-center justify-between">
           {/* Logo */}
           <div className="flex-shrink-0 pl-10">
-            <a href="#hero">
+            <a href="#hero" onClick={(e) => handleClick(e, "#hero")}>
               <h1 className="text-xl font-bold text-neutral-100">
                 Ice Parinwat
               </h1>
@@ -33,44 +51,47 @@ export default function Navbar() {
           {/* Navigation Links */}
           <div
             className={`
-              ${isMenuOpen ? "flex" : "hidden"} 
+              ${isMenuOpen ? "flex" : "hidden"}
               lg:flex
               backdrop-blur-sm
               bg-neutral-950
               absolute top-full right-0 
               lg:relative lg:top-auto lg:left-auto lg:right-10
-              flex-col lg:flex-row 
-              items-center            
+              flex-col lg:flex-row
+              items-center              
               gap-4 lg:gap-8
               lg:w-auto
               lg:bg-transparent
               pl-24 pr-24 pt-10 lg:p-0
               lg:h-0
               h-screen
-              mt-4 lg:mt-0
-              
+              mt-4 lg:mt-0             
             `}
           >
             <a
               href="#about"
+              onClick={(e) => handleClick(e, "#about")}
               className="text-neutral-300 hover:text-neutral-100 transition-colors"
             >
               About Me
             </a>
             <a
               href="#experience"
+              onClick={(e) => handleClick(e, "#experience")}
               className="text-neutral-300 hover:text-neutral-100 transition-colors"
             >
               Experience
             </a>
             <a
               href="#project"
+              onClick={(e) => handleClick(e, "#project")}
               className="text-neutral-300 hover:text-neutral-100 transition-colors"
             >
               Project
             </a>
             <a
               href="#contact"
+              onClick={(e) => handleClick(e, "#contact")}
               className="text-neutral-300 hover:text-neutral-100 transition-colors"
             >
               Contact
